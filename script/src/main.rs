fd302ffd5aa5b09fb884ff8da7aaea69ffbaa397
Walker-00 <w000alker@gmail.com>
2023-12-17 10:14:47 +0630

    "hackk"

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,13 +1,18 @@
-use std::process::Command;
+use std::{io::stdin, process::Command};
 
 fn main() {
-    Command::new("echo")
-        .args(["\"b\"", ">>", "d"])
-        .output()
-        .unwrap();
-    Command::new("git").args(["add", "-A"]).output().unwrap();
-    Command::new("git")
-        .args(["commit", "-m", "\"hackk\""])
-        .output()
-        .unwrap();
+    println!("How Much?: ");
+    let mut count = String::new();
+    stdin().read_line(&mut count).unwrap();
+    for _ in 0..count.trim().parse().unwrap() {
+        Command::new("echo")
+            .args(["\"b\"", ">>", "d"])
+            .output()
+            .unwrap();
+        Command::new("git").args(["add", "-A"]).output().unwrap();
+        Command::new("git")
+            .args(["commit", "-m", "\"hackk\""])
+            .output()
+            .unwrap();
+    }
 }
